e55343b8b993be30a58f4d234a1f800973b19da4
cytopia <cytopia@everythingcli.org>
2019-08-29 11:16:22 +0200

    OPS-0 CI Tests: check against non-strict versions

--- tests/support/run-tests.sh
+++ tests/support/run-tests.sh
@@ -8,52 +8,65 @@ script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 test_directory="${script_directory}/.."
 
 
-# --- Test if role fails when Helm command does NOT exist ---
-set +e
-# Test if helm was installed successfully
-ansible-playbook "${test_directory}/test-defaults.yml"
+if env | grep -q '^HELM_VERSION=' 2>/dev/null; then
+	HELM_VERSION="$( env | grep '^HELM_VERSION=' | sed 's/^HELM_VERSION=//g' )"
+else
+	HELM_VERSION="2.12.3"
+fi
 
-exit_code=$?
-if [ "${exit_code}" = '0' ]; then
+if env | grep -q '^ANSIBLE_ARGS=' 2>/dev/null; then
+	ANSIBLE_ARGS="--diff $( env | grep '^ANSIBLE_ARGS=' | sed 's/^ANSIBLE_ARGS=//g' )"
+else
+	ANSIBLE_ARGS="--diff"
+fi
+
+
+echo "${ANSIBLE_ARGS}"
+echo
+echo "----------------------------------------------------------------------------------------------------"
+echo "- [NEED TO FAIL] Helm is not installed"
+echo "----------------------------------------------------------------------------------------------------"
+# --- Test if role fails when Helm command does NOT exist ---
+# Test if helm was installed successfully
+if ansible-playbook ${ANSIBLE_ARGS} ${test_directory}/test-defaults.yml; then
   echo "Role should fail since helm command does NOT exist"
   exit 1
 fi
-set -e
-# ---
 
 
+echo
+echo "----------------------------------------------------------------------------------------------------"
+echo "- [NEED TO FAIL] Helm has wrong version"
+echo "----------------------------------------------------------------------------------------------------"
 # --- Test if role fails when specific Helm version does NOT exist ---
 # Mock Helm
 echo -e "#!/usr/bin/env bash" > /usr/local/bin/helm
-echo -e "echo '2.12.3-wrong-version'" >> /usr/local/bin/helm
+echo -e "echo '1-wrong-version'" >> /usr/local/bin/helm
 chmod u+x /usr/local/bin/helm
 
-set +e
 # Test if helm was installed successfully
-ansible-playbook "${test_directory}/test-defaults.yml"
-
-exit_code=$?
-if [ "${exit_code}" = '0' ]; then
-  echo "Role should fail since helm command does NOT exist"
+if ansible-playbook ${ANSIBLE_ARGS} ${test_directory}/test-defaults.yml; then
+  echo "Role should fail since helm command has wrong version"
   exit 1
 fi
-set -e
-# ---
 
 
-# --- Test if role works if no charts were defined ---
 # Mock Helm
 cp "${script_directory}/helm.mock" /usr/local/bin/helm
 
-# Run the role
-ansible-playbook "${test_directory}/test-defaults.yml"
-# ---
+
+echo
+echo "----------------------------------------------------------------------------------------------------"
+echo "- [NEED TO SUCC] Role without charts defined"
+echo "----------------------------------------------------------------------------------------------------"
+ansible-playbook ${ANSIBLE_ARGS} ${test_directory}/test-defaults.yml
 
 
-# --- Test if role works with charts ---
-# Run the role
-ansible-playbook "${test_directory}/test-with-charts.yml"
-# ---
+echo
+echo "----------------------------------------------------------------------------------------------------"
+echo "- [NEED TO SUCC] Role with charts defined"
+echo "----------------------------------------------------------------------------------------------------"
+ansible-playbook ${ANSIBLE_ARGS} ${test_directory}/test-with-charts.yml
 
 
 # Clean up
