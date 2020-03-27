6989dbb26ef4a5ae780d9fa3d1cedcb38254d810
vikkasyousaf <39026197+vikkasyousaf@users.noreply.github.com>
2020-03-27 09:50:04 +0100

    OPS-3624: upgrade helm to v3 (#13)

--- tests/support/run-tests.sh
+++ tests/support/run-tests.sh
@@ -8,7 +8,7 @@ script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 test_directory="${script_directory}/.."
 
 
-HELM_VERSION="${HELM_VERSION:-2.12.3}"
+HELM_VERSION="${HELM_VERSION:-3.1.2}"
 ANSIBLE_ARGS="--diff ${ANSIBLE_ARGS:-}"
 
 
