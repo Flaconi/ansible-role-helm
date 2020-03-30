c20d156be6f5cf7f775e8a882b0ceddd978e0432
Waqas Yousaf <waqasyousaf47@gmail.com>
2020-03-30 12:18:24 +0200

    OPS-0: add default repo

--- library/test_parse_helm_repositories.py
+++ library/test_parse_helm_repositories.py
@@ -9,7 +9,6 @@ class parse(unittest.TestCase):
     def test_parse_helm_repositories_output(self):
         helm_repositories_output = """
 NAME               	URL
-stable             	https://kubernetes-charts.storage.googleapis.com
 local              	http://127.0.0.1:8879/charts
 reactiveops-stable 	https://charts.reactiveops.com/stable
 flaconi-common-helm	s3://flaconi-helm-charts
@@ -17,10 +16,6 @@ zalenium-github    	https://raw.githubusercontent.com/zalando/zalenium/3.141.59u
         """.strip()
 
         expected_helm_repositories = [
-            {
-                "name": "stable",
-                "url": "https://kubernetes-charts.storage.googleapis.com",
-            },
             {
                 "name": "local",
                 "url": "http://127.0.0.1:8879/charts",
@@ -45,7 +40,6 @@ zalenium-github    	https://raw.githubusercontent.com/zalando/zalenium/3.141.59u
     def test_parse_helm_repositories_output_malformed(self):
         helm_repositories_output = """
 NAME               	VERSION URL
-stable             	1       https://kubernetes-charts.storage.googleapis.com
 local              	1       http://127.0.0.1:8879/charts
 reactiveops-stable 	1       https://charts.reactiveops.com/stable
 flaconi-common-helm	1       s3://flaconi-helm-charts
